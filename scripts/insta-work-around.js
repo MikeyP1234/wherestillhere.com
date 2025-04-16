c3ba4a52548c7318b0a48071748143cae5532b3d
Mikeyp1234 <51434401+MikeyP1234@users.noreply.github.com>
2025-04-15 22:59:39 -0400

    Update insta-work-around.js

--- scripts/insta-work-around.js
+++ scripts/insta-work-around.js
@@ -7,8 +7,8 @@ window.addEventListener("load", function () {
       <body style="background-color: black; margin: 0; display: flex; align-items: center; justify-content: center; height: 100vh; color: white; font-family: system; text-align: center;">
     <div>
       <p>I refuse to accommodate this poverty ass web browser </p>
-      <a href="https://wherestillhere.com" target="_blank" rel="noopener noreferrer" style="color: black; text-decoration: none; font-size: 25px; 
-        background-color:white; border-radius: 12px; padding:5px 15px;">
+      <a href="https://wherestillhere.com" target="_blank" rel="noopener noreferrer" style="color: black; text-decoration: none; font-size: 16px; 
+        background-color:white; border-radius: 12px; padding:5px 5px;">
         Hold Here, Click Link.
       </a>
       <p>Or just go to wherestillhere.com type shit</p>
