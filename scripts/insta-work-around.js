00287a94a5723ab61543d273c4cb11937bbfa030
Mikeyp1234 <51434401+MikeyP1234@users.noreply.github.com>
2025-04-15 22:23:47 -0400

    Update insta-work-around.js

--- scripts/insta-work-around.js
+++ scripts/insta-work-around.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const isInstagram = navigator.userAgent.toLowerCase().includes("instagram");
-  const video = document.getElementById("heroVideo");
-  const fallback = document.getElementById("videoFallback");
-  const viddiv = document.querySelector(".video-container");
-
   if (isInstagram && video && fallback) {
-    video.style.display = "none";
-    fallback.style.display = "block";
-    viddiv.style.height = window.innerHeight * 0.874 + "px";
+    document.body='';
+    document.body.style.backgroundColor = 'black';
   } 
   
 }
