69425e347fab2bd9a792b06a83219677b0d03d76
Mikeyp1234 <51434401+MikeyP1234@users.noreply.github.com>
2025-04-15 21:57:06 -0400

    Update insta-work-around.js
    instagram can do some things

--- scripts/insta-work-around.js
+++ scripts/insta-work-around.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const isInstagram = navigator.userAgent.toLowerCase().includes("instagram");
   const video = document.getElementById("heroVideo");
   const fallback = document.getElementById("videoFallback");
-  const container = document.querySelector(".video-container");
+  const viddiv = document.querySelector(".video-container");
 
   if (isInstagram && video && fallback) {
     video.style.display = "none";
     fallback.style.display = "block";
-    const viddiv=document.querySelector(".video-container");
-    viddiv.style.height = window.innerHeight * .874;
-  }
-  console.log(document.querySelector(".video-container"));
-
+    viddiv.style.height = window.innerHeight * 0.874 + "px";
+  } 
   
-});
+}
+);
