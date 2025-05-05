bb13dcff9dbe0462c42e5504bcbdd3caa315ef97
Mikeyp1234 <51434401+MikeyP1234@users.noreply.github.com>
2025-05-05 14:38:12 -0400

    Update insta-work-around.js
    removed swearing because its mean

--- scripts/insta-work-around.js
+++ scripts/insta-work-around.js
@@ -6,12 +6,12 @@ window.addEventListener("load", function () {
     document.documentElement.innerHTML = `
       <body style="background-color: black; margin: 0; display: flex; align-items: center; justify-content: center; height: 100vh; color: white; font-family: system; text-align: center;">
     <div>
-      <p>I refuse to accommodate this poverty ass web browser </p>
+      <p>I refuse to accommodate this web browser </p>
       <a href="https://wherestillhere.com" target="_blank" rel="noopener noreferrer" style="color: black; text-decoration: none; font-size: 16px; 
         background-color:white; border-radius: 12px; padding:5px 5px;">
         Hold Here.
       </a>
-      <p>Or just go to wherestillhere.com type shit</p>
+      <p>Or just go to wherestillhere.com</p>
     </div>
   </body>
     `;
